f81c2cf30eeedf6d6055a614b2b4ff00faa021e5
kth <rnjsxo1017@gmail.com>
2023-02-08 20:45:46 +0900

    애니메이션 연습 실패

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let styles = {
+    marginTop: '1000px' ,
+    color: 'white' ,
+    textAlign: 'center' ,
+    opacity: '1',
+    transition: 'all 0.5s'
+  }
+
+  let observer = new IntersectionObserver((e)=>{
+    e.forEach((box)=>{
+      box.target.style.color = 'black';
+    })
+  })
+  
+  let div = document.querySelectorAll('div');
+  
+  observer.observe(div[0]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div style={styles}>새로운 리액트 연습 시작</div>
+      <div style={styles}>이 페이지는 스크롤 애니메이션 구현</div>
+      <div style={styles}>구현 시작</div>
+      <div style={styles}>구현 끝</div>
+    </>
+  )
 }
 
 export default App;
